25d6b654e3351debfb02298e187685fc8f14ef54
marceljones <marceljones.mj@gmail.com>
2021-01-16 11:08:19 -0600

    added variables data types and math to main.js

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
-console.log("This is saturday morning classwork.")
+// console.log("CONNECTED")
+// //********START VARIABLES********* */
+// let firstName = 'Beyonce';
+// let lastName = 'Knowles';
+
+// console.log(firstName + lastName);
+// console.log(`${firstName} ${lastName}`)
+
+// firstName = 'Dr. T';
+// lastName = 'Vasquez';
+
+// console.log(firstName + lastName);
+// console.log(`${firstName} ${lastName}`)
+
+// //const favoriteFood = 'Tacos';
+// //console.log(favoriteFood)
+
+// //favoriteFood = 'Gumbo';
+// //Console.log(favoriteFood)
+// //***********END VARIABLES
+
+// //***************START DATA TYPES**************
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer = 33;
+// const negative = -14
+
+// console.log(typeof answer, typeof negative)
+
+// const iLoveJavascript = true;
+// console.log(typeof iLoveJavascript)
+// //*******END DATA TYPES******** */
+
+// //*******START MATH*******
+// console.log(Math.pow(2,2))
+// console.log(Math.pow(3,3))
+// console.log(Math.round(6.5))//7
+// console.log(Math.round(6.45))//6
+// console.log(Math.floor(6.99999))//6
+// console.log(Math.ceil(6.000000001))//7
+
+const cat ='kitty'
+console.log(cat.length) //5
+//*******END MATH******** */
