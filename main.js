3b0e3c0ca7a4cba735b54fd09f7a5f77647781bb
marceljones <marceljones.mj@gmail.com>
2021-01-16 11:58:08 -0600

    function examples

---
+++ main.js
@@ -0,0 +1,34 @@
+
+// function sayHello() {
+//   console.log("Hello")
+// }
+// sayHello()
+
+// const sayHello = function() {
+//   console.log("Hello!!")
+// }
+// sayHello()
+
+// const sayHello = () => {
+//   console.log("Hello!");
+// }
+// sayHello()
+
+//********FUNCTIONS WITH ARGUMENTS******
+
+// function logsHello(name) {
+//   console.log(`Hello, ${name}!`);
+  
+// }
+// logsHello('Marcy Jones')
+
+const sum = 55;
+const addsToNumbers = (num1, num2) => {
+  const sum = num1 + num2;
+  console.log(sum)
+  return sum
+}
+
+console.log(sum);
+console.log(addsToNumbers(2, 1));
+console.log(sum);
